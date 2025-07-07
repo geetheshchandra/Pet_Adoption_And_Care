dfcdff9363d1270232d70bdaf4b000b314078e7c
geetheshchandra <kgeethesh.chandra.cse23@itbhu.ac.in>
2025-07-07 19:06:56 +0530

    Configured static files for production

--- pawsitive_furpose/settings.py
+++ pawsitive_furpose/settings.py
@@ -54,6 +54,7 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,8 @@ STATICFILES_DIRS = [
    
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
