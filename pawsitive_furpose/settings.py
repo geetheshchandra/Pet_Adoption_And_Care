b6b622ebb18f6b9dd8509b180d5d9f084cca4783
geetheshchandra <kgeethesh.chandra.cse23@itbhu.ac.in>
2025-07-07 19:24:20 +0530

    Fix CSRF: added trusted origin

--- pawsitive_furpose/settings.py
+++ pawsitive_furpose/settings.py
@@ -140,6 +140,10 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://petadoptionandcare-production.up.railway.app',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
